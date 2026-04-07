88f299bacab21faece3c1ffea899ae254e87eb11
Aram Zand <47981723+aramzand@users.noreply.github.com>
2026-04-07 19:29:03 -0400

    Revert "Fixed duplicates"
    This reverts commit bec551c63e59bdce366de017b87a5845e1cd4d5d.

--- jablib/src/main/java/org/jabref/logic/cleanup/TimeStampToModificationDate.java
+++ jablib/src/main/java/org/jabref/logic/cleanup/TimeStampToModificationDate.java
@@ -1,10 +1,17 @@
 package org.jabref.logic.cleanup;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.jabref.logic.preferences.TimestampPreferences;
 import org.jabref.model.FieldChange;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.Date;
+import org.jabref.model.entry.event.EntriesEventSource;
 import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 
@@ -12,16 +19,14 @@ import org.jabref.model.entry.field.StandardField;
 ///
 /// If the old updateTimestamp setting is enabled, the timestamp field for each entry are migrated to the date-modified field.
 /// Otherwise it is migrated to the date-added field.
-public class TimeStampToModificationDate implements CleanupJob {
-
-    private final Field timeStampField;
+public class TimeStampToModificationDate extends AbstractTimeStampCleanup {
 
     public TimeStampToModificationDate(TimestampPreferences timestampPreferences) {
-        this.timeStampField = timestampPreferences.getTimestampField();
+        super(timestampPreferences);
     }
 
     @Override
-    public List<FieldChange> cleanup(BibEntry entry) {
-        return TimeStampMigrationHelper.migrate(entry, timeStampField, StandardField.MODIFICATIONDATE);
+    protected StandardField getTargetField() {
+        return StandardField.MODIFICATIONDATE;
     }
 }
